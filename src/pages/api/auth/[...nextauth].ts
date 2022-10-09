bdbd88bbb05f9a62f14231dc2c68d7172847783b
stamovm <m.stamov@yahoo.com>
2022-10-08 20:12:06 -0700

    Switch database to mysql Improved styling Added twitter authentication provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,29 +1,40 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from 'next-auth/providers/github'
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+import TwitterProvider from "next-auth/providers/twitter";
 
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '../../../server/db/client'
-import { env } from '../../../env/server.mjs'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
-        session.user.id = user.id
+        session.user.id = user.id;
       }
-      return session
+      return session;
     },
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
     GitHubProvider({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
+    }),
+    TwitterProvider({
+      clientId: env.TWITTER_ID,
+      clientSecret: env.TWITTER_SECRET,
+      version: "2.0", // opt-in to Twitter OAuth 2.0
     }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
@@ -34,6 +45,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,37 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { trpc } from '../utils/trpc'
-import { useRef } from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+import { useRef } from "react";
 
 const Home: NextPage = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery()
-  const inputRef = useRef<HTMLInputElement>(null)
-  const { data } = trpc.todo.getAll.useQuery()
-  const client = trpc.useContext()
-  const { data: session } = useSession()
+  // const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { data } = trpc.todo.getAll.useQuery();
+  const client = trpc.useContext();
+  const { data: session } = useSession();
 
   const create = trpc.todo.create.useMutation({
     onSuccess: () => {
-      client.todo.getAll.invalidate()
-      if (!inputRef.current) return
-      inputRef.current.value = ''
+      client.todo.getAll.invalidate();
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
     },
-  })
+  });
 
   const check = trpc.todo.updateChecked.useMutation({
     onSuccess: () => {
-      client.todo.getAll.invalidate()
+      client.todo.getAll.invalidate();
     },
-  })
+  });
   const del = trpc.todo.delete.useMutation({
     onSuccess: () => {
-      client.todo.getAll.invalidate()
+      client.todo.getAll.invalidate();
     },
-  })
+  });
 
   function handleCheck(e: { target: HTMLInputElement }) {
-    check.mutate({ id: e.target.id, checked: e.target.checked })
+    check.mutate({ id: e.target.id, checked: e.target.checked });
   }
 
   return (
@@ -41,12 +41,12 @@ const Home: NextPage = () => {
         <meta name="description" content="Todo list" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container flex flex-col items-center  min-h-screen p-4 mx-auto">
+      <main className="container mx-auto flex min-h-screen flex-col items-center p-4">
         <button
-          className="px-4 py-2 btn mx-4"
+          className="btn mx-4 px-4 py-2"
           onClick={session ? () => signOut() : () => signIn()}
         >
-          {session ? 'Sign out' : 'Sign in'}
+          {session ? "Sign out" : "Sign in"}
         </button>
 
         {session && (
@@ -54,31 +54,39 @@ const Home: NextPage = () => {
             <p className="text-2xl text-teal-500">
               Logged in as {session?.user?.name}
             </p>
-            <h1 className="pb-10 text-3xl  leading-normal font-bold text-teal-700">
+            <h1 className="mt-10 mb-4 text-3xl font-bold leading-normal text-teal-700">
               Todo List
             </h1>
-
-            <div className="p-3 border rounded">
-              <div className="pb-5 text-xl">
+            <div className="rounded-md border border-teal-500 p-3">
+              <div className="mb-2 text-xl">
                 {data ? (
                   data.map((todo) => (
                     <div
                       key={todo.id}
-                      className="text-xl pb-2 text-teal-500 hover:text-teal-700 duration-300"
+                      className="mb-1 flex  items-center  justify-between text-xl text-teal-500 duration-300 ease-in hover:text-teal-700 "
                     >
-                      <input
-                        type="checkbox"
-                        name="my"
-                        id={todo.id}
-                        onChange={handleCheck}
-                        checked={todo.checked}
-                      />
-                      <label htmlFor={todo.id}> {todo.text}</label>
+                      <div>
+                        <input
+                          className=" h-4 w-4 rounded-md border border-teal-300 bg-teal-500  checked:bg-teal-600 hover:cursor-pointer"
+                          type="checkbox"
+                          name="my"
+                          id={todo.id}
+                          onChange={handleCheck}
+                          checked={todo.checked}
+                        />
+                        <label
+                          className="hover:cursor-pointer"
+                          htmlFor={todo.id}
+                        >
+                          &nbsp;
+                          {todo.text}
+                        </label>
+                      </div>
                       <button
-                        className="bg-gray-200 hover:bg-gray-400  text-red-900 font-bold mx-2 px-2 rounded inline-flex items-center duration-300"
+                        className="ml-2 inline-flex items-center rounded-md bg-gray-200 px-2 text-sm font-bold text-red-900 duration-300 hover:bg-gray-400"
                         data-id={todo.id}
                         onClick={(e) => {
-                          del.mutate({ id: e.currentTarget.dataset.id || '' })
+                          del.mutate({ id: e.currentTarget.dataset.id || "" });
                         }}
                       >
                         X
@@ -93,23 +101,23 @@ const Home: NextPage = () => {
                 ref={inputRef}
                 type="text"
                 placeholder="Add todo"
-                className="p-2 border-2 rounded"
+                className="w-full rounded-md border border-teal-500 p-2"
                 disabled={create.isLoading}
                 onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    create.mutate({ text: e.currentTarget.value })
+                  if (e.key === "Enter") {
+                    create.mutate({ text: e.currentTarget.value });
                   }
                 }}
               />
             </div>
           </>
         )}
-        {session && secretMessage && (
+        {/* {session && secretMessage && (
           <p className="text-2xl text-teal-500">This: {secretMessage}</p>
-        )}
+        )} */}
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
